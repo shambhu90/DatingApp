a122db0bd94631a903f6ae782af6cb1824646c41
Shambhu <shambhu@deaninfotech.com>
2020-08-06 11:06:35 +0530

    Initial Version

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -24,7 +24,7 @@ namespace DatingApp.API
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
+        // This method gets called by the runtime. Use this method to add services to the container..
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<DataContext>(x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
